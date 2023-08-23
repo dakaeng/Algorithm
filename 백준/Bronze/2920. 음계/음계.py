2bac7c3b75cbc4308c9b458e83a4a1c592a8ea30
dakaeng <53594853+dakaeng@users.noreply.github.com>
2023-08-23 23:05:09 +0900

    [Bronze II] Title: 음계, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.py
@@ -0,0 +1,7 @@
+pitch = list(map(int, input().split()))
+if pitch == sorted(pitch) :
+    print('ascending')
+elif pitch == sorted(pitch, reverse = True) :
+    print('descending')
+else :
+    print('mixed')
