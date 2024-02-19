a191672ef50edc23d83f186077be8f5edad801c5
dakaeng <53594853+dakaeng@users.noreply.github.com>
2024-02-19 13:17:11 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 556 ms, Memory: 59404 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
@@ -0,0 +1,29 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+N, M, R = map(int, input().split())
+graph = [[] for _ in range(N+1)]
+for _ in range(M) :
+  u, v = map(int, input().split())
+  graph[u].append(v)
+  graph[v].append(u)
+
+def bfs(start) :
+  global count
+  queue = deque([start])
+  answer[start] = count
+  while queue :
+    v = queue.popleft()
+    graph[v].sort()
+    for i in graph[v] :
+      if answer[i] == 0 :
+        queue.append(i)
+        count += 1
+        answer[i] = count
+        
+answer = [0] * (N+1)
+count = 1
+bfs(R)
+for i in answer[1:] :
+  print(i)
