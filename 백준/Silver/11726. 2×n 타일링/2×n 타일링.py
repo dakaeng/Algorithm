0a9ffba946372b917c0ccecd5e029e5c374b2c5e
dakaeng <53594853+dakaeng@users.noreply.github.com>
2024-03-01 22:14:36 +0900

    [Silver III] Title: 2×n 타일링, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -0,0 +1,9 @@
+n = int(input())
+answer = [0] * (n+1)
+answer[1] = 1
+if n > 1 :
+  answer[2] = 2
+if n > 2 :
+  for i in range(3, n+1) :
+    answer[i] = (answer[i-1] + answer[i-2]) % 10007
+print(answer[n])
