e9b12f22862036d8a84dcc4c07c14ccc0c845480
dakaeng <53594853+dakaeng@users.noreply.github.com>
2024-04-23 12:22:17 +0900

    [Silver I] Title: 트리 순회, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -0,0 +1,29 @@
+N = int(input())  # 노드 개수
+tree = {}
+for _ in range(N) :
+  v, l, r = input().split()
+  tree[v] = [l, r]
+    
+def preorder(root) :
+  if root != '.' :
+    print(root, end = '')
+    preorder(tree[root][0])
+    preorder(tree[root][1])
+
+def inorder(root) :
+  if root != '.' :
+    inorder(tree[root][0])
+    print(root, end = '')
+    inorder(tree[root][1])
+
+def postorder(root) :
+  if root != '.' :
+    postorder(tree[root][0])
+    postorder(tree[root][1])
+    print(root, end = '')
+    
+preorder('A')
+print()
+inorder('A')
+print()
+postorder('A')
