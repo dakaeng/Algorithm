be4f330a746865ed0f8ae1aeacee936f4b483817
dakaeng <53594853+dakaeng@users.noreply.github.com>
2024-04-30 11:05:15 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,7 +1,19 @@
-from itertools import combinations
-
 N, M = map(int, input().split())
-combs = list(combinations(range(1, N+1), M))
-for c in combs :
-  for i in c :
-    print(i, end = ' ')
+
+def dfs(start) :
+  if len(s) == M :
+    print(' '.join(map(str, s)))
+    return
+
+  for i in range(start, N+1) :
+    if visited[i] == True :
+      continue
+    visited[i] = True
+    s.append(i)
+    dfs(i+1)
+    s.pop()
+    visited[i] = False
+    
+s = []
+visited = [False] * (N+1)
+dfs(1)
