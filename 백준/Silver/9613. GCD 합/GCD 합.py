7f4026e2c00caff642385238f405b4c24c54935b
dakaeng <53594853+dakaeng@users.noreply.github.com>
2024-07-03 15:59:01 +0900

    [Silver IV] Title: GCD 합, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9613. GCD 합/GCD 합.py
+++ 백준/Silver/9613. GCD 합/GCD 합.py
@@ -1,17 +1,18 @@
+# 두 수의 최대공약수 구하는 함수
 def GCD(n1, n2) :
-    rem = 1
-    while(rem != 0) :
-        rem = n1%n2
-        n1 = n2
-        n2 = rem
-    return n1
+  rem = 1
+  while (rem != 0) :
+    rem = n1 % n2  # n2로 나누어 떨어지면, n2가 곧 최대공약수
+    n1 = n2
+    n2 = rem
+  return n1  # n1 = n2 처리했기 때문
 
 t = int(input())
-for _ in range(t) :
-    n, *num = map(int, input().split())
 
-    answer = 0
-    for i in range(n-1) :
-        for j in range(i+1, n) :
-            answer += GCD(num[i], num[j])
-    print(answer)
+for _ in range(t) :
+  n, *nums = map(int, input().split())
+  answer = 0
+  for i in range(n-1) :
+    for j in range(i+1, n) :
+      answer += GCD(nums[i], nums[j])
+  print(answer)
