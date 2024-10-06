71c2a94191d4391561d30284d5e0e5cc835aaf0a
dakaeng <53594853+dakaeng@users.noreply.github.com>
2024-10-06 16:55:23 +0900

    [level 2] Title: H-Index, Time: 0.15 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,11 +1,15 @@
 def solution(citations):
     answer = 0
-    # [3, 0, 6, 1, 5] -> 3
-    count = [0] * (max(citations) + 1)
-    for i in citations :
-        count[i] += 1
-    for i in range(len(count)-1, -1, -1) :
-        if sum(count[i:]) >= i :
-            answer = i
+    n = len(citations)
+    citations.sort()
+    
+    count = 0
+    for i in range(n-1, -1, -1) :
+        count += 1
+        if citations[i] <= count :
+            answer = max(citations[i], count-1)
             break
+        elif count == n :
+            answer = n
+            
     return answer
