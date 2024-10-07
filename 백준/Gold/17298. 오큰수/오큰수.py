fab806f1e623b3444e2b4d91204efc8fabd7a14d
dakaeng <53594853+dakaeng@users.noreply.github.com>
2024-10-07 16:27:08 +0900

    [Gold IV] Title: 오큰수, Time: 996 ms, Memory: 155548 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.py
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -1,12 +1,13 @@
 N = int(input())
 A = list(map(int, input().split()))
 
-NGE = [-1] * N  # NGE[i] : A_i의 오큰수 저장
+# 스택 사용한 풀이 해보기
+answer = [-1] * N
 stack = []
 
 for i in range(N) :
-  while (stack and A[stack[-1]] < A[i]) :  # A[i]가 A[stack[-1]]의 오큰수가 되는 경우
-    NGE[stack[-1]] = A[i]  # 저장
-    stack.pop()  # A[stack[-1]]의 오큰수를 구했으므로 stack에서 해당 인덱스 값 제거
+  while (stack and A[stack[-1]] < A[i]) :
+    answer[stack[-1]] = A[i]
+    stack.pop()
   stack.append(i)
-print(*NGE)
+print(*answer)
