28ecdc4f01f18919472af7b84a692a19ec08a351
dakaeng <53594853+dakaeng@users.noreply.github.com>
2024-10-10 14:46:07 +0900

    [Silver III] Title: 계단 오르기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,16 +1,15 @@
-N = int(input())
-step = [0]
-for _ in range(N) :
-  step.append(int(input()))
+n = int(input())
+steps = []
+for _ in range(n) :
+  steps.append(int(input()))
 
-scores = [0 for _ in range(N+1)]
-if N >= 1 :
-  scores[1] = step[1]
-if N >= 2 :
-  scores[2] = step[1] + step[2]
-if N >= 3 :
-  scores[3] = max(step[1], step[2]) + step[3]
-if N > 3 :
-  for i in range(4, N+1) :
-    scores[i] = step[i] + max(step[i-1] + scores[i-3], scores[i-2])
-print(scores[N])
+d = [0 for _ in range(n)]
+d[0] = steps[0]
+if n > 1 :
+  d[1] = d[0] + steps[1]
+if n > 2 :
+  d[2] = max(d[0] + steps[2], steps[1] + steps[2])
+if n > 3 :
+  for i in range(3, n) : 
+    d[i] = steps[i] + max(steps[i-1] + d[i-3], d[i-2])
+print(d[-1])
