03f82257a2a123ed91c2b1d97b8b047c4f596ae8
dakaeng <53594853+dakaeng@users.noreply.github.com>
2024-10-11 14:24:52 +0900

    [Silver I] Title: 포도주 시식, Time: 272 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.py
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -1,17 +1,15 @@
 n = int(input())
-amount = []
-
+wine = []
 for _ in range(n) :
-  amount.append(int(input()))
+  wine.append(int(input()))
 
 d = [0] * n
-if n >= 1 :
-    d[0] = amount[0]   
-if n >= 2 :
-    d[1] = amount[0] + amount[1]
-if n >= 3 :
-    d[2] = max(amount[0] + amount[2], amount[1] + amount[2], d[1])  # 2번째 와인을 마시지 않는 경우도 고려
-if n >= 4 :
-    for i in range(3, n) :
-        d[i] = max(d[i-2] + amount[i], d[i-3] + amount[i-1] + amount[i], d[i-1])
+d[0] = wine[0]
+if n > 1 :
+  d[1] = d[0] + wine[1]
+if n > 2 :
+  d[2] = max(d[0] + wine[2], wine[1] + wine[2], d[1])  # 세번째 와인을 마시지 않는 경우도 고려
+if n > 3 :
+  for i in range(3, n) :
+    d[i] = max(d[i-2] + wine[i], wine[i-1] + d[i-3] + wine[i], d[i-1])  # (i+1)번째 와인을 마시지 않는 경우도 고려
 print(max(d))
